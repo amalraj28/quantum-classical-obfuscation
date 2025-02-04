b9ea16273b52f6ec038e50a6620c998983f47097
Amal Raj <amalraj2821@gmail.com>
2025-02-04 18:30:01 +0800

    Encoder completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
+import matplotlib.pyplot as plt
+from stack import Stack
 
 def create_circuit(n: int):
-    pass
     qc = QuantumCircuit(n)
 
     print("Available gates: X, H, CX (CNOT), Z, S, T, SWAP")
@@ -15,6 +16,8 @@ def create_circuit(n: int):
         "T": qc.t,
         "CX": qc.cx,
         "SWAP": qc.swap,
+        "CSWAP": qc.cswap,
+        "CCX": qc.ccx,
     }
 
     while True:
@@ -36,35 +39,104 @@ def create_circuit(n: int):
             qc.cx(control, target)
 
         elif gate == "SWAP":
-            q1 = int(input("Enter the first qubit: ")) # Handle edge cases
-            q2 = int(input("Enter the second qubit: ")) # Handle edge cases
+            q1 = int(input("Enter the first qubit: "))  # Handle edge cases
+            q2 = int(input("Enter the second qubit: "))  # Handle edge cases
             gates[gate](q1, q2)
 
         else:
             print("Invalid gate. Please choose from the available options.")
-        
-        qc.measure_all()
 
     return qc
+
 
 def compile_and_run(circuit: QuantumCircuit):
     simulator = AerSimulator()
     transpiled = transpile(circuit, simulator)
     result = simulator.run(transpiled, shots=1024).result()
-    print(result.get_counts())
+    return transpiled, result.get_counts()
 
-    # return transpiled, result.get_counts()
-    return [], []
 
-# n = int(input("Enter the number of qubits: "))  # Handle edge cases
-# qc = create_circuit(n)
+def draw(qc: QuantumCircuit, output="mpl"):
+    qc.draw(output=output)
+    plt.show()
 
-qc = QuantumCircuit(3)
-qc.x(1)
-qc.h(0)
-qc.cx(1, 2)
+
+def add_classical_gate(qc: QuantumCircuit, gate: str, qubits: list[int]):
+    gate = gate.upper()
+    gates = {
+        "X": qc.x,
+        "Y": qc.y,
+        "H": qc.h,
+        "Z": qc.z,
+        "S": qc.s,
+        "T": qc.t,
+        "CX": qc.cx,
+        "SWAP": qc.swap,
+        "CSWAP": qc.cswap,
+        "CCX": qc.ccx,
+    }
+
+    if gate not in ["CX", "CCX", "SWAP", "CSWAP"]:
+        raise ValueError(f"Invalid gate {gate}")
+
+    if gate in ["CX", "SWAP"]:
+        if len(qubits) != 2:
+            raise ValueError("Number of qubits must be exactly 2")
+
+        gates[gate](*qubits)
+
+    else:
+        if len(qubits) != 3:
+            raise ValueError("Number of qubits must be exactly 3")
+
+        gates[gate](*qubits)
+
+    return qc
+
+
+def parse_string(string: str):
+    if len(string) == 0:
+        return
+
+    gate, *qubits = string.split()
+    gate = gate.upper()
+    qubits = list(map(lambda x: int(x), qubits))
+
+    return gate, qubits
+
+
+n = int(input("Enter the number of qubits: "))  # Handle edge cases
+qc = create_circuit(n)
+valid_gates = ["CX", "CCX", "SWAP", "CSWAP"]
+gates = {
+    "X": qc.x,
+    "Y": qc.y,
+    "H": qc.h,
+    "Z": qc.z,
+    "S": qc.s,
+    "T": qc.t,
+    "CX": qc.cx,
+    "SWAP": qc.swap,
+    "CSWAP": qc.cswap,
+    "CCX": qc.ccx,
+}
+
+stack = Stack()
+
+while True:
+    prompt = input(
+        "Enter the gates and qubit to be added at the end of circuit, separated by space (leave blank for exiting): "
+    )
+    if len(prompt) == 0:
+        break
+
+    gate, qubits = parse_string(prompt)
+    qc = add_classical_gate(qc, gate, qubits)
+    stack.push((gate, qubits))
+
 qc.measure_all()
 
-# transpiled, result = compile_and_run(qc)
+transpiled, results = compile_and_run(qc)
 
-qc.draw(output='mpl')
+measurements = results.keys()
+
---
+++ stack.py
@@ -0,0 +1,22 @@
+from queue import LifoQueue
+
+class Stack:
+    def __init__(self):
+        self.st = LifoQueue(0)
+    
+    def push(self, item):
+        self.st.put_nowait(item)
+    
+    def pop(self):
+        self.st.get_nowait()
+    
+    def top(self):
+        item = self.st.get_nowait()
+        self.push(item)
+        return item
+    
+    def size(self):
+        return self.st.qsize()
+    
+    def empty(self):
+        return self.st.empty()
