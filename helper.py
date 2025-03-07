9d497e05adf4a0052932041af9490bdde9528014
Amal Raj <amalraj2821@gmail.com>
2025-03-07 13:53:40 +0800

    Added function to generate random filename

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 from qiskit.qasm2 import dump as q2_dump, load as q2_load
 from qiskit.qasm3 import dump as q3_dump, load as q3_load
 from qiskit import QuantumCircuit
+from uuid import uuid4
 
 class Input:
     @staticmethod
@@ -43,3 +44,6 @@ def write_qasm2(qc: QuantumCircuit, file_name: str):
     
     with open(file_name, 'w') as file:
         q2_dump(qc, file)
+
+def generate_unique_string() -> str:
+    return str(uuid4()).replace('-', '')
